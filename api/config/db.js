5ae5232357948e90a4e033ecbf5b7d0dd73a693b
Zunair-arhamsoft <zunair.saeed@arhamsoft.com>
2025-04-22 13:09:27 +0500

    Minor changes for railway deployment

--- api/config/db.js
+++ api/config/db.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
+const { dbURL } = require('./vars');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(dbURL, {
     dialect: 'postgres',
-    logging: false,
+    dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false }
+    }
 });
 
 module.exports = sequelize;
--- api/config/vars.js
+++ api/config/vars.js
@@ -4,6 +4,7 @@ module.exports = {
     dbPass: process.env.DB_PASS,
     dbHost: process.env.DB_HOST,
     jwtSecret: process.env.JWT_SECRET,
-    port: process.env.PORT
+    port: process.env.PORT,
+    dbURL: process.env.DB_URL,
 
 }
