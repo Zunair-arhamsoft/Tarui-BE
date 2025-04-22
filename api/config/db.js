cac9ff8ffcfd2d8a8b45d266ad88f5d0544eba66
Zunair-arhamsoft <zunair.saeed@arhamsoft.com>
2025-04-22 13:17:44 +0500

    db url var changes

--- api/config/db.js
+++ api/config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
-const { dbURL } = require('./vars');
+// const { dbURL } = require('./vars');
 
-const sequelize = new Sequelize(dbURL, {
+const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: { require: true, rejectUnauthorized: false }
