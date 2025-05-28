2d691eab826cd40dc6cbcf231673f7ff0f6dda62
Zunair-arhamsoft <zunair.saeed@arhamsoft.com>
2025-05-28 18:29:56 +0500

    Vercel deployment setup

--- api/index.js
+++ api/index.js
@@ -7,14 +7,10 @@ const { port } = require('./vars');
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-}));
-app.options('*', cors());
+app.use(cors());
 
 app.use(express.json());
-app.use('/api', routes);
+app.use('/api/', routes);
 
 app.get('/', async (req, res) => {
     try {
